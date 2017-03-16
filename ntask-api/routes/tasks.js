b89fcd45de4fd46fd1d6a05fdb03b49d79ffceff
Sergio Rojas <sergiorojas@qdata.io>
2017-03-16 18:14:43 -0500

    CRUD for users done, and corrections to the tasks

--- ntask-api/routes/tasks.js
+++ ntask-api/routes/tasks.js
@@ -38,7 +38,7 @@ module.exports = app => {
         });
     })
     .delete((req, res) => {
-        Tasks.destroy({where: req.params.id})
+        Tasks.destroy({where: {id: req.params.id}})
         .then(result => res.sendStatus(204))
         .catch(error => {
             res.status(412).json({msg: error.message});

98d3893f8016c56e20876e265e2355495eecedc1
Sergio Rojas <sergiorojas@qdata.io>
2017-03-16 15:39:24 -0500

    database implemented and middleware started

--- ntask-api/routes/tasks.js
+++ ntask-api/routes/tasks.js
@@ -1,8 +1,28 @@
 module.exports = app => {
-    const Tasks = app.models.tasks;
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}, (tasks) => {
-            res.json({tasks: tasks});
+    const Tasks = app.db.models.Tasks;
+    app.route("/tasks")
+    .all((req, res) => {
+        
+    })
+    .get( (req, res) => {
+        Tasks.findAll({})
+        .then( result => res.json(result))
+        .catch(error => {
+            res.status(412).json({msg: error.message});
         });
+    })
+    .post((req, res) => {
+
+    });
+
+    app.route("/routes/:id")
+    .all((req, res) => {
+
+    })
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+
     });
 };

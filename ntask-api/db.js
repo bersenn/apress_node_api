6697ceef8b6ffdc5989f736976eff9754ff2b60b
Sergio Rojas <sergiorojas@qdata.io>
2017-03-16 09:02:55 -0500

    Module one ended and database module started

---
+++ ntask-api/db.js
@@ -0,0 +1,5 @@
+import Sequelize from "sequelize";
+const config = require(".libs/config.js");
+let sequelize = null;
+
+module.exports = ()
--- ntask-api/routes/tasks.js
+++ ntask-api/routes/tasks.js
@@ -1,12 +1,8 @@
 module.exports = app => {
+    const Tasks = app.models.tasks;
     app.get("/tasks", (req, res) => {
-        res.json(
-            {
-                tasks: [
-                    {title: "Buy some shoes"},
-                    {title: "Fix notebook"}
-                ]
-            }
-        );
+        Tasks.findAll({}, (tasks) => {
+            res.json({tasks: tasks});
+        });
     });
 };
